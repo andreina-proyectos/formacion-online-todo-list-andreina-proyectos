4d2aaaa058314bd743a777e25afb63bc304d6ecc
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-24 19:44:42 +0100

    Add listener to modal add task button and take input value

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const buttonToAddTask = document.querySelector('.button__add-task');
 const toDoList = document.querySelector('.main__list-to-do');
 const finishedList = document.querySelector('.main__list-finished');
 const modal = document.querySelector('.page__modal');
+const modalTaskInput = document.querySelector('.modal__input');
+const modalAddTaskButton = document.querySelector('.modal__button');
 
 
 function handleClickAddButton(event) {
   modal.classList.remove('hidden');
 }
 
+function handleClickModalAddBtn(event) {
+  const newTaskToAdd = modalTaskInput.value;
+  console.log(newTaskToAdd);
+}
+
 //listeners
 buttonToAddTask.addEventListener('click', handleClickAddButton);
+modalAddTaskButton.addEventListener('click', handleClickModalAddBtn);
