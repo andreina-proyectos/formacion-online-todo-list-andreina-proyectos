995b08707e7dcce94dec20ba48907b2a27de664c
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-24 19:37:57 +0100

    Remove hidden class to modal when button is clicked

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const buttonToAddTask = document.querySelector('.button__add-task');
+const toDoList = document.querySelector('.main__list-to-do');
+const finishedList = document.querySelector('.main__list-finished');
+const modal = document.querySelector('.page__modal');
+
+
+function handleClickAddButton(event) {
+  modal.classList.remove('hidden');
+}
+
+//listeners
+buttonToAddTask.addEventListener('click', handleClickAddButton);
